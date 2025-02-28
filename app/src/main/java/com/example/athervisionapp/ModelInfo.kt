f3b37be6394a05989dff4c95919b618f99269ab7
mahadevanpv <pvmahdevan.1991@gmail.com>
2025-02-28 15:27:08 +0530

    athervisionv1

--- app/src/main/java/com/example/athervisionapp/ModelInfo.kt
+++ app/src/main/java/com/example/athervisionapp/ModelInfo.kt
@@ -7,8 +7,8 @@ object ModelInfo {
     const val MODEL_HEIGHT = 640
 
     // Add these constants to ModelInfo.kt
-    const val OUTPUT_SIZE = 300  // The number of detection results
-    const val OUTPUT_FEATURES = 6  // The number of features per detection (x, y, w, h, confidence, class)
+    const val OUTPUT_SIZE = 6  // The number of detection results
+    const val OUTPUT_FEATURES = 300  // The number of features per detection (x, y, w, h, confidence, class)
 
     // COCO dataset labels
     val LABELS = listOf(
--- app/src/main/java/com/example/athervisionapp/ObjectDetector.kt
+++ app/src/main/java/com/example/athervisionapp/ObjectDetector.kt
@@ -1,5 +1,6 @@
 package com.example.athervisionapp
 
+
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.RectF
