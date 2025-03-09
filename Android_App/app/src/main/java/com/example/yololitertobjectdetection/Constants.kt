47706beaaf0453cb051d7acef7646706ebd90189
mahadevanpv <pvmahdevan.1991@gmail.com>
2025-03-09 18:17:43 +0530

    athervisionfinal

--- Android_App/app/src/main/java/com/example/yololitertobjectdetection/Constants.kt
+++ Android_App/app/src/main/java/com/example/yololitertobjectdetection/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.yololitertobjectdetection
 
 object Constants {
-    const val MODEL_PATH = "yolov10s_float16.tflite"
+    const val MODEL_PATH = "yolov12n_float16.tflite"
 
     val LABELS_PATH: String? = null // provide your labels.txt file if the metadata not present in the model
 }
